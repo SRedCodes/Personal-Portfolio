6fc6ac330819a26f33c76b9248624032ea5cea4e
Srireddy <srika.kodes@gmail.com>
2023-12-13 17:23:52 +0530

    declaration of functions in 3 ways in JS

--- JavaScript/functions/js/main.js
+++ JavaScript/functions/js/main.js
@@ -5,4 +5,18 @@ function getUserNameFromEmail(email) {
     return email.slice(0,email.indexOf('@'));
 
 }
- console.log(getUserNameFromEmail("player@gmail.com"));
+ console.log(getUserNameFromEmail("pOne@gmail.com"));
+
+ 
+ //anonymous function
+ let getUserNameFromMail = function(mail){
+    return mail.slice(0,mail.indexOf("@"));
+ }
+ console.log(getUserNameFromMail('bittu@gmail'));
+ 
+ 
+ //arrow function
+ const toProperCase = (name) =>{
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+ };
+ console.log(toProperCase("sriKANtH"));
