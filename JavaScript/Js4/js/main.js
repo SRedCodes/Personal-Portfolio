9b96621a0c3aa324c4ff72662391d3d9e4ea859f
Srikanth Reddy <srika.kodes@gmail.com>
2024-01-10 10:10:21 +0530

    Delete JavaScript directory

--- JavaScript/Js4/js/main.js
+++
@@ -1,7 +0,0 @@
-const myNumber=42;
-const myString="42.124ads";
-const myFloat=42.155;
-
-console.log(Number.parseFloat(myFloat).toFixed(2));
-console.log(Number.parseFloat(myString));
-console.log(Number.parseInt(myFloat));
